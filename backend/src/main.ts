8cab650e875eb7ea0d3c2bd0bec62b5308857a15
Dmitry Firsanov <dmirtyfirsanov03@gmail.com>
2024-11-27 20:38:13 +0300

    feat: добавлен swagger для описания api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('yacp API')
+    .setDescription('Документация для yacp API')
+    .setVersion('0.0.1')
+    .addTag('yacp')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
